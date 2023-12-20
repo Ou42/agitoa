dbdc582ba3539761b71e1847f36977d67da77dc4
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-12-20 00:02:49 -0800

    updated separationParser
    - short is `-s`
    - long is `--separation`

--- app/Main.hs
+++ app/Main.hs
@@ -31,34 +31,45 @@ appOptionsParser =
         <*> probsInGroupParser
         <*> outputParser
         <*> inputFileParser
-    where
-        withAnswersParser :: Parser Bool
-        withAnswersParser =
-          switch $
-            short 'a'
-              <> long "with-answers"
-              <> help "Generate problem answers"
-        maxDigitsParser :: Parser Int
-        maxDigitsParser =
-          option auto $
-            short 'd'
-              <> long "max-digits"
-              <> metavar "Int"
-              <> help "Maximum number of digits allowed in a term."
-              <> value 4
-        separationParser :: Parser Int
-        separationParser = pure 4
-        probsInGroupParser :: Parser Int
-        probsInGroupParser = pure 5
-        outputParser :: Parser AppOutput
-        outputParser =
-          option (OutputFile <$> str) $
-            short 'o'
-              <> long "output-file"
-              <> metavar "FILE"
-              <> help
-                  "Output file for the arranged problems. \
-                  \ If not given, output is written to standard output."
-              <> value StdOut
-        inputFileParser :: Parser FilePath
-        inputFileParser = pure "input.txt"
+
+withAnswersParser :: Parser Bool
+withAnswersParser =
+  switch $
+    short 'a'
+      <> long "with-answers"
+      <> help "Generate problem answers"
+
+maxDigitsParser :: Parser Int
+maxDigitsParser =
+  option auto $
+    short 'd'
+      <> long "max-digits"
+      <> metavar "Int"
+      <> help "Maximum number of digits allowed in a term."
+      <> value 4
+
+separationParser :: Parser Int
+separationParser =
+  option auto $
+    short 's'
+      <> long "separation"
+      <> metavar "Int"
+      <> help "Number of spaces between vertically arranged problems."
+      <> value 4
+
+probsInGroupParser :: Parser Int
+probsInGroupParser = pure 5
+
+outputParser :: Parser AppOutput
+outputParser =
+  option (OutputFile <$> str) $
+    short 'o'
+      <> long "output-file"
+      <> metavar "FILE"
+      <> help
+          "Output file for the arranged problems. \
+          \ If not given, output is written to standard output."
+      <> value StdOut
+
+inputFileParser :: Parser FilePath
+inputFileParser = pure "input.txt"
